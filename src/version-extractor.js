358ca03785e50c44bb6782cf3fcf66a02ced5ba8
jyeany <jyeany@gmail.com>
2022-06-05 12:55:27 -0600

    Print outputs for extract version

--- src/version-extractor.js
+++ src/version-extractor.js
@@ -3,11 +3,14 @@ import { exec } from "child_process";
 export default function extractVersion() {
   exec("./gradlew properties | grep 'version:'", (error, stdout, stderr) => {
     if (error) {
+      console.log(error)
       throw error;
     }
     if (stderr) {
+      console.log(stderr)
       throw stderr;
     }
+    console.log(stdout)
     return stdout.split(':')[1].trim()
   });
 }
