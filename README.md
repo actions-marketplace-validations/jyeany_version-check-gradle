b11c7fb9042d1a77c4368685944ffc8fa8fb78d3
jyeany <jyeany@gmail.com>
2022-06-05 11:10:01 -0600

    Minor README cleanup. Print gradle properties

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ This action checks to see if a package with the current version has already been
 
 ## Outputs
 
-## `changed`: either "true" or "false", indicates if the version has been published
-## `version`: if the version changed, it shows the new version number
+`changed`: either "true" or "false", indicates if the version has been published  
+`version`: if the version changed, it shows the new version number
 
 ## Example usage
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
 const core = require('@actions/core');
+const { exec } = require("child_process");
 // const github = require('@actions/github');
 
 console.log('main.js run');
 try {
   core.setOutput("changed", true);
   core.setOutput("version", "any");
+
+  exec("./gradlew properties", (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+
 } catch (error) {
   core.setFailed(error.message);
 }
