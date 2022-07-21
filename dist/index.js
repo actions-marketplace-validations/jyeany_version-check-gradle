b3d86ff31477762f414be0a839120a78c8ad95b9
jyeany <jyeany@gmail.com>
2022-07-21 13:10:11 -0600

    Move repeated get request into function. try/catch around the request.

--- dist/index.js
+++ dist/index.js
@@ -8243,38 +8243,42 @@ function execCallback(resolve, reject, error, stdout, stderr, cmd) {
 class VersionClient {
 
   async isVersionPresent(packageName, version, orgName, accessToken) {
-    try {
-      if (orgName) {
-        return this.findVersionForOrganization(packageName, version, orgName, accessToken);
-      } else {
-        return this.findVersionForUser(packageName, version, accessToken);
-      }
-    } catch (ex) {
-      console.log(ex);
-      return false;
+    if (orgName) {
+      return this.findVersionForOrganization(packageName, version, orgName, accessToken);
+    } else {
+      return this.findVersionForUser(packageName, version, accessToken);
     }
   }
 
   async findVersionForOrganization(packageName, version, orgName, accessToken) {
     const reqUrl = `https://api.github.com/orgs/${orgName}/packages/maven/${packageName}/versions`;
-    const res = await axios__WEBPACK_IMPORTED_MODULE_0__.get(reqUrl, {
-        headers: {
-          'Authorization': `Bearer ${accessToken}`,
-          'Accept': 'application/vnd.github.v3+json'
-        }
-      });
-    return this.checkIfVersionIn(version, res.data);
+    try {
+      const res = await this.makeRequest(reqUrl, accessToken);
+      return this.checkIfVersionIn(version, res.data);
+    } catch (err) {
+      console.log(`status: ${err.response.status}`);
+      return true;
+    }
   }
 
   async findVersionForUser(packageName, version, accessToken) {
     const reqUrl = `https://api.github.com/users/jyeany/packages/maven/${packageName}/versions`;
-    const res = await axios__WEBPACK_IMPORTED_MODULE_0__.get(reqUrl, {
+    try {
+      const res = await this.makeRequest(reqUrl, accessToken);
+      return this.checkIfVersionIn(version, res.data);
+    } catch (err) {
+      console.log(`status: ${err.response.status}`);
+      return true;
+    }
+  }
+
+  async makeRequest(reqUrl, accessToken) {
+    return axios__WEBPACK_IMPORTED_MODULE_0__.get(reqUrl, {
         headers: {
           'Authorization': `Bearer ${accessToken}`,
           'Accept': 'application/vnd.github.v3+json'
         }
       });
-    return this.checkIfVersionIn(version, res.data);
   }
 
   checkIfVersionIn(version, packageVersions) {
